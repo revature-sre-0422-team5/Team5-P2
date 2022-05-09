fd9bb9ce4259be376ef3bc89cc7c062e6aa20257
daewoon865 <102994015+daewoon865@users.noreply.github.com>
2022-05-09 19:37:50 -0400

    Add files via upload

---
+++ deliveryApi/src/test/java/com/team5/deliveryApi/DeliveryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team5.deliveryApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
