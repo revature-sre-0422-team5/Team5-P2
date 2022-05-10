7d087577cf744ed1bac151d1982bda0292d3aced
daewoon Kim <daewoon865@revature.net>
2022-05-10 00:11:58 -0400

    initial commits

---
+++ deliveryApi/src/main/java/com/team5/entities/OrderItem.java
@@ -0,0 +1,14 @@
+package com.team5.entities;
+
+import javax.persistence.Entity;
+
+@Entity
+public class OrderItem {
+
+    private long itemId;
+
+    private String itemName;
+
+    private String itemDescription;
+
+}
