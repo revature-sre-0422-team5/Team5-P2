16d153b4cd1cb2f71b4081aed525d96e0e21e84d
FruityPear <jty2114@gmail.com>
2022-05-09 20:50:55 -0400

    Added email services

---
+++ notificationApi/src/main/java/com/team5/notificationApi/NotificationApiApplication.java
@@ -0,0 +1,12 @@
+package com.team5.notificationApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationApiApplication.class, args);
+	}
+}
