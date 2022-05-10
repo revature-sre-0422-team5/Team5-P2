afd4a17ed42519ae41a4a9a69960d3cf55ebcdbd
daewoon Kim <daewoon865@revature.net>
2022-05-10 14:30:15 -0400

    adjust gitignore, modified more orderitem entity

--- deliveryApi/src/main/java/com/team5/entities/OrderItem.java
+++ deliveryApi/src/main/java/com/team5/entities/OrderItem.java
@@ -1,14 +1,25 @@
 package com.team5.entities;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import lombok.*;
 
 @Entity
+@Table (name="OrderItems")
+@Getter
+@Setter
+@ToString
+@Builder
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderItem {
 
     private long itemId;
 
+    @Column (name="itemName")
     private String itemName;
 
+    @Column (name="itemDescription")
     private String itemDescription;
 
 }
