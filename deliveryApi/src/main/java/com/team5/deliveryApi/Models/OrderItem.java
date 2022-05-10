b9706fd87f8cabf045ac0953812bca52d414d30d
daewoon Kim <daewoon865@revature.net>
2022-05-10 18:22:35 -0400

    refactor

--- deliveryApi/src/main/java/com/team5/deliveryApi/Models/OrderItem.java
+++ deliveryApi/src/main/java/com/team5/deliveryApi/Models/OrderItem.java
@@ -1,4 +1,4 @@
-package com.team5.entities;
+package com.team5.deliveryApi.Models;
 
 import javax.persistence.*;
 import lombok.*;
@@ -14,6 +14,9 @@ import lombok.*;
 @AllArgsConstructor
 public class OrderItem {
 
+    @Id
+    @Column (name ="itemId")
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long itemId;
 
     @Column (name="itemName")
