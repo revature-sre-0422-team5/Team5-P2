3076916f80503a7abfaeb523afada11f33653dc7
daewoon Kim <daewoon865@revature.net>
2022-05-10 22:40:49 -0400

    Some cru functions for order items

---
+++ deliveryApi/src/main/java/com/team5/deliveryApi/dto/OrderItemAdd.java
@@ -0,0 +1,12 @@
+package com.team5.deliveryApi.dto;
+
+import lombok.*;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderItemAdd {
+    private String itemName;
+
+    private String itemDescription;
+}
