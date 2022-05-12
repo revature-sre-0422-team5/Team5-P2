10b05b92588ee97ece9f43685813149df0f7eb80
Tony <32178366+tonykwak96@users.noreply.github.com>
2022-05-11 23:44:49 -0400

    WIP

--- deliveryApi/src/main/java/com/team5/deliveryApi/Controllers/OrderController.java
+++ deliveryApi/src/main/java/com/team5/deliveryApi/Controllers/OrderController.java
@@ -3,15 +3,10 @@ package com.team5.deliveryApi.Controllers;
 import com.team5.deliveryApi.Services.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 import com.team5.deliveryApi.Models.Order;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/Order")
@@ -51,4 +46,8 @@ public class OrderController {
         return orderService.viewStatusById(id);
     }
 
+    @PutMapping("/pay/{id}")
+    public ResponseEntity payOrderById(@PathVariable int id){
+        return ResponseEntity.ok(orderService.payOrder(id));
+    }
 }
