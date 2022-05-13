497d42443eb7bd9af05c214c262ce06a8ab5bfee
daewoon865 <102994015+daewoon865@users.noreply.github.com>
2022-05-13 15:27:18 -0400

    Add files via upload

---
+++ deliveryApi/src/main/java/com/team5/deliveryApi/services/CustomerService.java
@@ -0,0 +1,48 @@
+package com.team5.deliveryApi.services;
+
+import com.team5.deliveryApi.Models.Customer;
+import com.team5.deliveryApi.repositories.CustomerRepository;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CustomerService {
+
+        private CustomerRepository customerRepository;
+
+        public CustomerService(CustomerRepository customerRepository) {
+            super();
+            this.customerRepository = customerRepository;
+        }
+
+
+        public boolean saveCustomer(Customer incominngCustomer) {
+            customerRepository.save(incominngCustomer);
+            return true;
+        }
+
+        public ResponseEntity viewAllCustomer(){
+            return ResponseEntity.ok(customerRepository.findAll());
+        }
+    
+        public Optional<Customer> viewCustomerById(int id){
+            return customerRepository.findById(id);
+        }
+
+        public ResponseEntity subscribeEmailById(int id){
+            customerRepository.updateEmailSubscriptionById("yes",id);
+            return ResponseEntity.ok(true);
+        }
+
+        public ResponseEntity unsubscribeEmailById(int id){
+            customerRepository.updateEmailSubscriptionById("no",id);
+            return ResponseEntity.ok(true);
+        }
+
+
+    }
+
