e59530dcd713f4e29bd7d70bd2aa68e8a7bf139a
daewoon865 <102994015+daewoon865@users.noreply.github.com>
2022-05-13 15:24:40 -0400

    Add files via upload

---
+++ deliveryApi/src/main/java/com/team5/deliveryApi/controllers/CustomerController.java
@@ -0,0 +1,51 @@
+package com.team5.deliveryApi.controllers;
+
+import com.team5.deliveryApi.Models.Customer;
+import com.team5.deliveryApi.services.CustomerService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/Customer")
+public class CustomerController {
+        @Autowired
+        CustomerService customerService;
+
+        @PostMapping("/new")
+        public String createCustomerAccount(@RequestBody Customer incomingCustomer) {
+
+            /** To get logging message*/
+            Logger logger = LoggerFactory.getLogger(CustomerController.class);
+            boolean success = customerService.saveCustomer(incomingCustomer);
+            logger.info("Adding new Customer");
+            logger.info("incoming Customer"+ incomingCustomer);
+            if (success ==true) {
+                return "Account created successfully";
+
+            } else {
+                return "Error in creating new Customer Account";
+            }
+        }
+
+        @GetMapping("/all")
+        public ResponseEntity viewAllCustomer(){
+            return customerService.viewAllCustomer();
+        }
+
+        @PutMapping("/subscribe/{id}")
+        public ResponseEntity subscribeEmailById(@PathVariable int id){
+            return  customerService.subscribeEmailById(id);
+        }
+
+
+        @PutMapping("/unsubscribe/{id}")
+        public ResponseEntity unsubscribeEmailById(@PathVariable int id){
+            return  customerService.unsubscribeEmailById(id);
+        }
+
+}
