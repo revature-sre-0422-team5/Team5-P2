8eec1ef83af72575d2aa1f77154f9b6fc88f6b64
daewoon865 <102994015+daewoon865@users.noreply.github.com>
2022-05-13 15:28:25 -0400

    Add files via upload

---
+++ deliveryApi/src/main/java/com/team5/deliveryApi/repositories/ItemRepository.java
@@ -0,0 +1,9 @@
+package com.team5.deliveryApi.repositories;
+
+import com.team5.deliveryApi.Models.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends JpaRepository<Item,Integer> {
+}
