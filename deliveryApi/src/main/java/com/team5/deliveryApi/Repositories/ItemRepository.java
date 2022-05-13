6c093e5316b1b51e7eb6f1abc24fe2a771b5457d
daewoon865 <102994015+daewoon865@users.noreply.github.com>
2022-05-13 15:28:38 -0400

    Delete deliveryApi/src/main/java/com/team5/deliveryApi/Repositories directory

--- deliveryApi/src/main/java/com/team5/deliveryApi/Repositories/ItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.team5.deliveryApi.repositories;
-
-import com.team5.deliveryApi.Models.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ItemRepository extends JpaRepository<Item,Integer> {
-}
