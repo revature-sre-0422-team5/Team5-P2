dc3cff3c2b5c757094a98c47dac18e286d010475
Roshney123 <roshney.kuriakose@gmail.com>
2022-05-16 09:46:42 -0400

    Order modified

--- deliveryApi/src/main/java/com/team5/deliveryApi/controllers/OrderController.java
+++ deliveryApi/src/main/java/com/team5/deliveryApi/controllers/OrderController.java
@@ -1,11 +1,9 @@
 package com.team5.deliveryApi.controllers;
 
+import com.team5.deliveryApi.models.Item;
 import com.team5.deliveryApi.models.Order;
 import com.team5.deliveryApi.services.OrderService;
-import com.team5.deliveryApi.dto.Item;
 import com.team5.deliveryApi.dto.OrderLocation;
-import com.team5.deliveryApi.models.Order;
-import com.team5.deliveryApi.services.OrderService;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,31 +18,12 @@ public class OrderController {
     @Autowired
     private OrderService orderService;
 
-    @PostMapping("/new")
-    public String createOrder(@RequestBody Order incomingOrder) {
-
-
-        log.info("Creating new Order");
-        boolean success = orderService.saveOrder(incomingOrder);
-        log.info("incoming order" + incomingOrder);
-        if (success == true) {
-            return "Successfully created new order";
-        } else {
-            return "Error in creating new Order";
-        }
-    }
 
     @GetMapping("/all")
     public ResponseEntity viewAllOrders(){
         return orderService.viewAllOrders();
     }
     
-    @GetMapping("/viewId/{odr_id}")
-    public Order viewRbById(@PathVariable int odr_id) {
-        Order outGoingOrder = orderService.findByOrderId(odr_id);
-        return outGoingOrder;
-    }
-
 
     @GetMapping("/status/{id}")
     public ResponseEntity viewStatusById(@PathVariable int id) {
@@ -57,49 +36,73 @@ public class OrderController {
     }
 
 
-     @RequestMapping(value = "/addLocationDescription/{odr_id}", method = RequestMethod.PUT)
-     public Order updateLocationOrder(@RequestBody OrderLocation orderLocation, @PathVariable int odr_id) {
-         Order Out=orderService.updateLocation(orderService.findByOrderId(odr_id),orderLocation);
-         return Out;
+
+
+    @PostMapping("/new")
+    public ResponseEntity createOrder(@RequestBody Order incomingOrder) {
+
+        log.info("Creating new Order");
+        boolean success = orderService.saveOrder(incomingOrder);
+        log.info("incoming order" + incomingOrder);
+        if (success == true) {
+            return ResponseEntity.ok().body("Successfully created new order");
+        } else {
+            return ResponseEntity.ok().body("Error in creating new Order");
+        }
+    }
+
+    @GetMapping("/viewId/{odrId}")
+    public ResponseEntity<Order> viewOrderById(@PathVariable int odrId) {
+        Order outGoingOrder = orderService.findByOrderId(odrId);
+        return ResponseEntity.ok().body(outGoingOrder);
+
+    }
+
+    @RequestMapping(value = "/addLocationDescription/{odrId}", method = RequestMethod.PUT)
+     public ResponseEntity<Order>  updateLocationOrder(@RequestBody OrderLocation orderLocation, @PathVariable int odrId) {
+         Order Out=orderService.updateLocation(orderService.findByOrderId(odrId),orderLocation);
+         return ResponseEntity.ok().body(Out);
     }
 
 
-    @RequestMapping(value = "/removeItem/{odr_id}/{item_id}", method = RequestMethod.PUT)
-    public String removeItem(@PathVariable int odr_id,@PathVariable int item_id) {
+    @RequestMapping(value = "/removeItem/{odrId}/{itemId}", method = RequestMethod.PUT)
+    public ResponseEntity removeItem(@PathVariable int odrId,@PathVariable int itemId) {
 
-       boolean success=orderService.removeItem(orderService.findByOrderId(odr_id),item_id);
+       boolean success=orderService.removeItem(orderService.findByOrderId(odrId),itemId);
        if (success==true) {
-           return "Successfully Removed the item";
+           return ResponseEntity.ok().body( "Successfully Removed the item");
        }else{
-           return "Error in removing item";
+           return ResponseEntity.ok().body("Error in removing item");
        }
 
 
     }
-   @RequestMapping(value = "/addItem/{odr_id}/{item}", method = RequestMethod.PUT)
-   public Order addItem(@PathVariable int odr_id, @PathVariable  int item, @RequestBody Item dto_item) {
-       Order Out=orderService.addItem(orderService.findByOrderId(odr_id),item,dto_item);
-       return Out;
+   @RequestMapping(value = "/addItem/{odrId}/{groceryItemID}", method = RequestMethod.POST)
+  public ResponseEntity <Order> addItem(@PathVariable int odrId, @PathVariable int groceryItemID, @RequestParam("qty") int quantity) {
+        log.info("checking"+odrId+groceryItemID+quantity);
+       Order order=orderService.addItem(odrId,groceryItemID,quantity);
+       log.info("in controller");
+       return ResponseEntity.ok().body(order);
 
    }
 
-   @RequestMapping(value="/submitOrder/{odr_id}", method = RequestMethod.PUT)
-    public String submitOrder(@PathVariable int odr_id){
-         boolean success= orderService.submitOrder(orderService.findByOrderId(odr_id));
+   @RequestMapping(value="/submitOrder/{odrId}", method = RequestMethod.PUT)
+    public ResponseEntity submitOrder(@PathVariable int odrId){
+         boolean success= orderService.submitOrder(orderService.findByOrderId(odrId));
         if (success==true) {
-            return "Order submitted Successfully";
+            return ResponseEntity.ok().body("Order submitted Successfully");
         }else{
-            return "Error in submitting order";
+            return ResponseEntity.ok().body("Error in submitting order");
         }
     }
-    @DeleteMapping("/delete/{odr_id}")
-    public String cancelOrder(@PathVariable int odr_id) {
-         boolean success=orderService.deleteOrder(orderService.findByOrderId(odr_id));
+    @DeleteMapping("/delete/{odrId}")
+    public ResponseEntity cancelOrder(@PathVariable int odrId) {
+         boolean success=orderService.deleteOrder(orderService.findByOrderId(odrId));
          if(success==true) {
-             return "Order cancelled Successfully";
+             return ResponseEntity.ok().body("Order cancelled Successfully");
          }
          else{
-             return "Error in cancelling Order";
+             return ResponseEntity.ok().body("Error in cancelling Order");
          }
     }
 }
--- deliveryApi/src/main/java/com/team5/deliveryApi/services/OrderService.java
+++ deliveryApi/src/main/java/com/team5/deliveryApi/services/OrderService.java
@@ -1,32 +1,43 @@
 package com.team5.deliveryApi.services;
 
+import com.team5.deliveryApi.dto.ItemStatus;
+import com.team5.deliveryApi.models.GroceryItem;
+import com.team5.deliveryApi.models.Item;
+import com.team5.deliveryApi.models.ItemNotFoundException;
 import com.team5.deliveryApi.models.Order;
-import com.team5.deliveryApi.dto.Item;
 import com.team5.deliveryApi.dto.OrderLocation;
+import com.team5.deliveryApi.repositories.GroceryItemRepository;
+import com.team5.deliveryApi.repositories.ItemRepository;
 import com.team5.deliveryApi.repositories.OrderRepository;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+@Slf4j
 @Service
 public class OrderService {
 
     private OrderRepository orderRepository;
-
+    private GroceryItemRepository groceryItemRepository;
+    private ItemRepository itemRepository;
+    Logger logger = LoggerFactory.getLogger(OrderService.class);
     public OrderService(OrderRepository orderRepository){
         super();
         this.orderRepository = orderRepository;
     }
 
+
+
     public ResponseEntity viewAllOrders(){
         return ResponseEntity.ok(orderRepository.findAll());
     }
 
-    public ResponseEntity viewOrderById(int id){
-        return ResponseEntity.ok(orderRepository.findById(id));
-    }
+
 
     public ResponseEntity viewStatusById(int id){
         return ResponseEntity.ok(orderRepository.findById(id).getStatus());
@@ -37,16 +48,25 @@ public class OrderService {
         return true;
     }
 
+
+
+
+
+
     public boolean saveOrder(Order incomingOrder) {
         orderRepository.save(incomingOrder);
         return true;
     }
-   public Order findByOrderId(int odr_id) {
+    public Order viewOrderById(int id){
+        Order outGoingOrder=(orderRepository.findById(id));
+        return outGoingOrder;
+    }
+   public Order findByOrderId(int odrId) {
 
-        Logger logger = LoggerFactory.getLogger(OrderService.class);
+
         logger.info("Getting Order by Id");
 
-        Order outGoingOrder = orderRepository.findById(odr_id);
+        Order outGoingOrder = orderRepository.findById(odrId);
 
         if (outGoingOrder != null) {
 
@@ -56,6 +76,7 @@ public class OrderService {
     }
 
     public Order updateLocation(Order incomingOrder, OrderLocation incomingLocation){
+
         incomingOrder.setFrom_location(incomingLocation.getDto_from_location());
         incomingOrder.setDescription(incomingLocation.getDto_description());
         Order updatedOrder=orderRepository.save(incomingOrder);
@@ -64,29 +85,25 @@ public class OrderService {
 
 
     public boolean removeItem(Order incomingOrder,int item_Id){
-        /*
-        if(incomingOrder.getItem_Id()==item_Id) {
-            incomingOrder.setItem_Id(0);
-            incomingOrder.setItem(null);
-            incomingOrder.setItem_description(null);
+
+        /*Optional<Item> cartItem = incomingOrder.getItems().stream().filter(i -> i.getId() == item_Id).findFirst();
+
+           // itemRepository.delete(cartItem);*/
             orderRepository.save(incomingOrder);
             return true;
-        }
-        else{return false;}
 
-         */
-        return true;
     }
-    public Order addItem(Order incomingOrder, int item, Item dto_item) {
-        /*
-          incomingOrder.setItem_Id(item);
-          incomingOrder.setItem(dto_item.getProductName());
-          incomingOrder.setItem_description(dto_item.getProductDescription());
-          Order updatedOrder=orderRepository.save(incomingOrder);
-          return updatedOrder;
 
-         */
-        return null;
+    public Order addItem(int odrID,int gItemID,int qnty) {
+          log.info("checking"+odrID+gItemID+qnty);
+          GroceryItem groceryItem=groceryItemRepository.findById(gItemID).get();
+          log.info("Checking Grocery Item"+groceryItem);
+          Item item =new Item(qnty, ItemStatus.Added,groceryItem);
+          logger.info("item"+item);
+          Order addedOrder=orderRepository.findById(odrID);
+          addedOrder.getItems().add(item);
+          orderRepository.save(addedOrder);
+          return (addedOrder);
     }
 
 
