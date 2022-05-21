0c22dcb562d162cef8dcbee2247371e390dd466d
Tony <32178366+tonykwak96@users.noreply.github.com>
2022-05-21 16:42:12 -0400

    Create more Test

--- deliveryApi/src/main/java/com/team5/deliveryApi/controllers/ShopperController.java
+++ deliveryApi/src/main/java/com/team5/deliveryApi/controllers/ShopperController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/Shopper")
+@RequestMapping("/shopper")
 public class ShopperController {
     @Autowired
     ShopperService shopperService;
---
+++ deliveryApi/src/test/java/com/team5/deliveryApi/controllers/GroceryControllerTest.java
@@ -0,0 +1,4 @@
+package com.team5.deliveryApi.controllers;
+
+public class GroceryControllerTest {
+}
