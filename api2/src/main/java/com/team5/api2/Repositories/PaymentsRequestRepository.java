5a4f88c2e9f5f9c01035302956b263de605e0745
Daewoon Kim <daewoon865@revature.net>
2022-05-20 23:43:03 -0400

    fixed slf4j, created event for updating payment status after being redirected from stripe checkout, changed payOrder implementation

--- api2/src/main/java/com/team5/api2/Repositories/PaymentsRequestRepository.java
+++ api2/src/main/java/com/team5/api2/Repositories/PaymentsRequestRepository.java
@@ -1,11 +1,15 @@
 package com.team5.api2.Repositories;
 
+import java.util.List;
+
 import com.team5.api2.models.OrderPaymentEntity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PaymentsRequestRepository extends JpaRepository<OrderPaymentEntity,Integer>{
-    
+    @Query (value = "SELECT * from order_payments u where u.stripe_charge_id = ?1", nativeQuery = true)
+    List<OrderPaymentEntity> findByStripeId (String stripeChargeId);
 }
